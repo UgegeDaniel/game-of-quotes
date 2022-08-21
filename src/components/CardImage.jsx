4eef93305cd616a6ac5859bd553d009e1cadc101
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-08-21 12:52:32 +0100

    Update CardImage.jsx

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -4,12 +4,10 @@ import gotLogo from '../img/gotLogo.jpg'
 const CardImage = ({character}) => {
     return (
         <div className='img-container'>
-            <div className='img-container'>
-                <img src={character?.imageUrl || gotLogo} alt={"character i"} className='person-img' />
-                <span className='quote-icon'>
-                    <FaQuoteRight />
-                </span>
-            </div>
+           <img src={character?.imageUrl || gotLogo} alt={"character i"} className='person-img' />
+              <span className='quote-icon'>
+                 <FaQuoteRight />
+              </span>
         </div>
     )
 }
