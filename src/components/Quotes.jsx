e40bf423e9c222e574722dad1dca97b3fb90cac0
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-08-21 12:49:15 +0100

    Update Quotes.jsx

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 const Quotes = ({character, quote}) => {
     return (
-    <>
+    <div>
         <h4 className='author'>{character?.name}</h4>
         <p className='job'>{character?.house?.name ? character?.house?.name : 'House unknown'}</p>
         <p className='info'>{quote}</p>
-    </>
+    </div>
 
     )
 }
