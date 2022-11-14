b954b1b9a1ecd70df64e2c6c309bfed2f12ae13f
Ugege Daniel <ugege62@gmail.com>
2022-11-14 08:57:48 +0100

    eslint

--- src/utils/fetchUrlData.js
+++ src/utils/fetchUrlData.js
@@ -1,13 +1,14 @@
 const fetchUrlData = async (url, onStart, onSuccess, onError) => {
-    onStart();
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      onSuccess(data);
-    } catch (error) {
-      onError(error.message)
-      console.log(error)
-    }
+  onStart();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    onSuccess(data);
+  } catch (error) {
+    onError(error.message);
+    // eslint-disable-next-line no-console
+    console.log(error);
   }
+};
 
 export default fetchUrlData;
